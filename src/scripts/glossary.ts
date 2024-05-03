e14cb8d71df8611931f2f51b2435ae3402d2598d
Henry Wilder <henrythepony@gmail.com>
2024-05-03 16:48:43 -0400

    Greatly improved page appearance

--- src/scripts/glossary.ts
+++ src/scripts/glossary.ts
@@ -1,4 +1,4 @@
-import type { CollectionEntry, ValidContentEntrySlug } from 'astro:content';
+import { getCollection, getEntry, type CollectionEntry, type ValidContentEntrySlug } from 'astro:content';
 
 export const kinds = ['function', 'type', 'field', 'parameter', 'return', 'constant', 'trait', 'module'] as const;
 export type Kind = (typeof kinds)[number];
@@ -6,3 +6,17 @@ export type Kind = (typeof kinds)[number];
 export type GlossaryEntry = CollectionEntry<'glossary'>;
 export type ValidGlossarySlug = ValidContentEntrySlug<'glossary'>;
 export type GlossaryRef = { collection: 'glossary'; slug: ValidGlossarySlug };
+
+export const getChildren = async (parent: ValidGlossarySlug, filter?: (entry: GlossaryEntry) => boolean): Promise<GlossaryEntry[]> => {
+    const parentPath = parent.split('/');
+    return await getCollection('glossary', (entry: GlossaryEntry) => {
+        const path = entry.slug.split('/');
+        const isChild = path.length === parentPath.length + 1 && path.slice(0, -1).every((part, index) => part === parentPath[index]);
+        return isChild && (!filter || filter(entry));
+    });
+};
+
+export const getParent = async (item: GlossaryEntry): Promise<GlossaryEntry | undefined> => {
+    const parentSlug = item.slug.split('/').slice(0, -1).join('/');
+    return parentSlug.length !== 0 ? await getEntry('glossary', parentSlug as ValidGlossarySlug) : undefined;
+};
